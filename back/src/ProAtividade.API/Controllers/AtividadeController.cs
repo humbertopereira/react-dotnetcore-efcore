bd6df8a840d22a8d2918db4c384bdcf6ac2bcdce
Humberto Pereira <humbertopereira@gmail.com>
2022-07-25 21:14:55 -0300

    Seção 03 - EF Core - Integração

--- back/src/ProAtividade.API/Controllers/AtividadeController.cs
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Data;
 using ProAtividade.API.Models;
 
 namespace ProAtividade.API.Controllers
@@ -10,6 +11,13 @@ namespace ProAtividade.API.Controllers
     [Route("api/[controller]")]
     public class AtividadeController : Controller
     {
+        private readonly ILogger<AtividadeController> logger;
+        private readonly DataContext context;
+        public AtividadeController(ILogger<AtividadeController> logger, DataContext context)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
         private IEnumerable<Atividade> Atividades = new List<Atividade>(){
             new Atividade(1),
             new Atividade(2),
@@ -19,32 +27,50 @@ namespace ProAtividade.API.Controllers
         [HttpGet]
         public IEnumerable<Atividade> Get()
         {
-            return Atividades;
+            return context.Atividades;
         }
 
         [HttpGet("{id}")]
         public Atividade Get(int id)
         {
-            return Atividades.FirstOrDefault(e => e.Id == id);
+            return context.Atividades.FirstOrDefault(e => e.Id == id);
         }
 
         [HttpPost]
         public IEnumerable<Atividade> Post([FromBody] Atividade atividade)
         {
-            return Atividades.Append<Atividade>(atividade);
+            context.Atividades.Add(atividade);
+
+            if (context.SaveChanges() > 0)
+                return context.Atividades;
+            else
+                throw new Exception("Você não conseguiu adicionar uma atividade");
         }
 
         [HttpPut("{id}")]
         public Atividade Put(int id, Atividade atividade)
         {
-            atividade.Id = atividade.Id + 1;
-            return atividade;
+            if (atividade.Id != id) throw new Exception("Você esta tentando atualizar uma atividade errada");
+
+            context.Update(atividade);
+
+            if (context.SaveChanges() > 0)
+                return context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
+            else
+                throw new Exception("Você não conseguiu adicionar uma atividade");
         }
 
         [HttpDelete("{id}")]
-        public string Delete(int id)
+        public bool Delete(int id)
         {
-            return "Meu primeiro método Delete";
+            var atividade = context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
+
+            if (atividade == null)
+                throw new Exception("Você esta tentando deletar uma atividade que não existe.");
+
+            context.Atividades.Remove(atividade);
+
+            return context.SaveChanges() > 0;
         }
     }
 }
--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,10 +1,22 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using ProAtividade.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager Configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(Configuration.GetConnectionString("Default"))
+);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+    options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
